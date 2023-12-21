a634e39c997df49ad449981bde7714dfed787526
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-21 22:58:02 +0100

    added personal website logo to navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -23,6 +23,25 @@ const Navbar = () => {
 			</h1>
 			<div className="icons">
 				<a
+					href="https://www.instagram.com/codecr8ive"
+					target="_blank"
+					rel="noopener">
+					<Image
+						src={"/images/instagram.svg"}
+						width={25}
+						height={25}
+						alt="instagram icon"
+					/>
+				</a>
+				<a href="https://codecr8ive.pl/" target="_blank" rel="noopener">
+					<Image
+						src={"/images/logo.svg"}
+						width={25}
+						height={25}
+						alt="github icon"
+					/>
+				</a>
+				<a
 					href="https://github.com/WojciechStancel"
 					target="_blank"
 					rel="noopener">
@@ -33,6 +52,7 @@ const Navbar = () => {
 						alt="github icon"
 					/>
 				</a>
+
 				<a
 					href="https://www.facebook.com/profile.php?id=61552481912036"
 					target="_blank"
@@ -42,17 +62,6 @@ const Navbar = () => {
 						width={25}
 						height={25}
 						alt="facebook icon"
-					/>
-				</a>
-				<a
-					href="https://www.instagram.com/codecr8ive"
-					target="_blank"
-					rel="noopener">
-					<Image
-						src={"/images/instagram.svg"}
-						width={25}
-						height={25}
-						alt="instagram icon"
 					/>
 				</a>
 			</div>
