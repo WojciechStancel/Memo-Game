900ef3bd9d68d40ebc21787b824ae1611657b5c2
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-21 14:16:11 +0100

    added socials to navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 					alt="aircraft icon"
 					className="planeLeft"
 				/>{" "}
-				Welcome in Aviation Memory Game
+				Aviation Memory Game
 				<Image
 					src="images/plane.svg"
 					width={30}
@@ -21,6 +21,41 @@ const Navbar = () => {
 					className="planeRight"
 				/>
 			</h1>
+			<div className="icons">
+				<a
+					href="https://github.com/WojciechStancel"
+					target="_blank"
+					rel="noopener">
+					<Image
+						src={"/images/github.svg"}
+						width={25}
+						height={25}
+						alt="github icon"
+					/>
+				</a>
+				<a
+					href="https://www.facebook.com/profile.php?id=61552481912036"
+					target="_blank"
+					rel="noopener">
+					<Image
+						src={"/images/facebook.svg"}
+						width={25}
+						height={25}
+						alt="facebook icon"
+					/>
+				</a>
+				<a
+					href="https://www.instagram.com/codecr8ive"
+					target="_blank"
+					rel="noopener">
+					<Image
+						src={"/images/instagram.svg"}
+						width={25}
+						height={25}
+						alt="instagram icon"
+					/>
+				</a>
+			</div>
 		</div>
 	);
 };
